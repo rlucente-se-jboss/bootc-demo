2f0120149ff2343d665ba4cf39593ea497450433
rlucente-se-jboss <rlucente@redhat.com>
2024-05-13 17:19:38 -0400

    use SCA credentials to register the system

--- register-and-update.sh
+++ register-and-update.sh
@@ -4,6 +4,13 @@
 
 [[ $EUID -ne 0 ]] && exit_on_error "Must run as root"
 
+subscription-manager register \
+    --username $SCA_USER --password $SCA_PASS || exit 1
+subscription-manager role --set="Red Hat Enterprise Linux Server"
+subscription-manager service-level --set="Self-Support"
+subscription-manager usage --set="Development/Test"
+subscription-manager attach
+
 dnf -y update
 dnf -y clean all
 
