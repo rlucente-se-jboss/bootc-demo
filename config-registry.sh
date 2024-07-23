01c745d979340d508fb8b19200dd0474eca5cf7f
rlucente-se-jboss <rlucente@redhat.com>
2024-07-23 19:50:13 -0400

    add local registry

---
+++ config-registry.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/demo.conf
+
+[[ $EUID -ne 0 ]] && exit_on_error "Must run as root"
+
+#
+# Setup for a local insecure registry
+#
+firewall-cmd --permanent --add-port=5000/tcp
+firewall-cmd --reload
+
+cat > /etc/containers/registries.conf.d/999-local-registry.conf <<EOF
+[[registry]]
+insecure = true
+blocked = false
+location = "$HOSTIP:5000"
+EOF
+
+#
+# Create quadlet for registry service
+#
+mkdir -p /var/lib/registry
+
+cat > /etc/containers/systemd/local-registry.container <<EOF
+[Unit]
+Description=A simple local registry
+
+[Container]
+Image=docker.io/library/registry:2
+ContainerName=registry
+PublishPort=5000:5000
+Volume=/var/lib/registry:/var/lib/registry:Z
+
+[Service]
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+
+#
+# Launch the local registry
+#
+systemctl daemon-reload
+systemctl start local-registry
