088d2b489fa23ef7e9b38153ce303e5c7d4505d6
rlucente-se-jboss <rlucente@redhat.com>
2024-11-11 13:54:00 -0500

    use syspurpose correctly

--- register-and-update.sh
+++ register-and-update.sh
@@ -6,7 +6,7 @@
 
 subscription-manager register \
     --username $SCA_USER --password $SCA_PASS || exit 1
-subscription-manager syspurpose role --set="RHEL Server"
+subscription-manager syspurpose role --set="Red Hat Enterprise Linux Server"
 subscription-manager syspurpose service-level --set="Self-Support"
 subscription-manager syspurpose usage --set="Development/Test"
 
